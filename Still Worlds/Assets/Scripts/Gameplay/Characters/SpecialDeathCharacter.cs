9a26d69b317ad7c883dbf20e1327e20a48d63e27
Nikola Ćališ <nidjolas.u.b@gmail.com>
2021-03-01 07:10:42 +0100

    Finish convert scene

---
+++ Still Worlds/Assets/Scripts/Gameplay/Characters/SpecialDeathCharacter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Characters
+{
+    public class SpecialDeathCharacter : NPCCharacter
+    {
+        public GameObject portal;
+
+        public override void SpecialAction()
+        {
+            portal.SetActive(true);
+            ICharacter playerCharacter = GameObject.FindGameObjectWithTag("Player").GetComponent<ICharacter>();
+            playerCharacter.SetLife(playerCharacter.GetLife() + playerCharacter.GetKill());
+            playerCharacter.SetKill(0);
+            Destroy(gameObject);
+        }
+    }
+}
