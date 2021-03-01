8af072697a20c28d9fddb916a1af7959b8174dff
Nikola Ćališ <nidjolas.u.b@gmail.com>
2021-03-01 03:34:14 +0100

    Finish heart scene

---
+++ Still Worlds/Assets/Scripts/Gameplay/Characters/LifeCharacter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Characters
+{
+    public class LifeCharacter : NPCCharacter
+    {
+        public GameObject portal;
+        public int newLifeValue = 5;
+
+        public override void SpecialAction()
+        {
+            portal.SetActive(true);
+            GameObject.FindGameObjectWithTag("Player").GetComponent<ICharacter>().SetLife(newLifeValue);
+            Destroy(gameObject);
+        }
+    }
+}
+
